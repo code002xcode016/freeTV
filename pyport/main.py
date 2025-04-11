d9b771af8250ca093697a302acffc5edfbbf3661
code002xcode016 <hasrinismail@Hasrins-MacBook-Pro.local>
2025-04-11 16:05:21 +0800

    Skibidi

--- pyport/main.py
+++ pyport/main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import tkinter as tk
+import mpv
+
+
+
+
+root = tk.Tk()
+
+frame = tk.Frame(root, width=700, height=600)
+frame.pack()
+
+display = tk.Frame(frame, bd=5)
+display.place(relwidth=1, relheight=1)
+#player.set_xwindow(display.winfo_id())
+#player.set_nsobject(display.winfo_id())
+player = mpv.MPV(log_handler=print , ytdl=True , wid=str(display.winfo_id()) , hwdec="auto")
+
+player.play("/Users/hasrinismail/Downloads/freeetv/pyport/hachi.mp4")
+
+
+root.mainloop()
